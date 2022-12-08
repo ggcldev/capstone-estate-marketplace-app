6c1a0c7a5615961912309a2f37fe0ee41d0b7a13
ggcldev <ggcldev@gmail.com>
2022-12-08 14:07:00 +0800

    Worked on sign-in option

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/kbArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn() {
+  // * State for show password, email and data
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  // * Destructure for global use
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+      </div>
+    </>
   )
 }
 
