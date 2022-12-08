6c8f002703e657bb2441aae35ad28d5cbecb31d2
ggcldev <ggcldev@gmail.com>
2022-12-08 17:07:47 +0800

    Working on Sign in Form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { ReactComponent as ArrowRightIcon } from '../assets/svg/kbArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+// import { ReactComponent as ArrowRightIcon } from '../assets/svg/kbArrowRightIcon.svg'
+// import { ReactComponent as visibilityIcon } from '../assets/svg/visibleIcon.svg'
+import visibilityIcon from '../assets/svg/visibleIcon.svg'
 
 function SignIn() {
-  // * State for show password, email and data
+  // * State for email and data
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
     email: '',
@@ -14,6 +15,7 @@ function SignIn() {
   const { email, password } = formData
 
   const navigate = useNavigate()
+  const onChange = () => {}
 
   return (
     <>
@@ -21,6 +23,36 @@ function SignIn() {
         <header>
           <p className='pageHeader'>Welcome Back!</p>
         </header>
+        <form>
+          {/* Email input */}
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+          {/* Password input */}
+          <div className='passwordInputDiv'>
+            <input
+              // if show password is true return text input else password
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={(e) => setShowPassword(e.target.value)}
+            />
+
+            <img
+              src={visibilityIcon}
+              alt='visibleIcon'
+              className='showPassword'
+              onClick={() => setShowPassword()}
+            />
+          </div>
+        </form>
       </div>
     </>
   )
