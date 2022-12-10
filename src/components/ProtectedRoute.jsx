7f0b0c6492704a8662094f081bb65837274f2c54
ggcldev <ggcldev@gmail.com>
2022-12-10 17:18:35 +0800

    Added useAuthStatus: to check if use is sign-in or not

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
 
-const PrivateRoute = () => {
-  const loggedIn = false
+const ProtectedRoute = () => {
+  // ! Check status if login
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
+  if (checkingStatus) {
+    return <h3>Loading...</h3>
+  }
+  // ! If not login, will redirect to sign in page
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
-export default PrivateRoute
+export default ProtectedRoute
