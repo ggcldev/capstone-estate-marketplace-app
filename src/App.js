1b04dd1e0fb27bb68c7c0d9a4b6d187bf387df7f
ggcldev <ggcldev@gmail.com>
2022-12-10 17:07:08 +0800

    Initialize Protected Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
+import ProtectedRoute from './components/ProtectedRoute'
 import Discover from './pages/Discover'
 import ForgotPassword from './pages/ForgotPassword'
 import Profile from './pages/Profile'
@@ -17,7 +18,9 @@ function App() {
           {/* Pages Route */}
           <Route path='/' element={<Discover />} />
           <Route path='/sales' element={<Sales />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<ProtectedRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
