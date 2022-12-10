9a682fb2bc0d107cf85a7e3b46046baf8527caf9
ggcldev <ggcldev@gmail.com>
2022-12-10 17:28:20 +0800

    Added spinner

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
 const ProtectedRoute = () => {
   // ! Check status if login
   const { loggedIn, checkingStatus } = useAuthStatus()
 
   if (checkingStatus) {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
   // ! If not login, will redirect to sign in page
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
