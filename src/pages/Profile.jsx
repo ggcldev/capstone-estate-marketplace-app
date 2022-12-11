989e8b0e1c051739f7efcf9d19bbcb0d93c5785b
ggcldev <ggcldev@gmail.com>
2022-12-12 03:01:43 +0800

    Added delete function on profile listing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import { getAuth, updateProfile } from 'firebase/auth'
+import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+
+import { getAuth, updateProfile } from 'firebase/auth'
 import { db } from '../firebase.config'
-import { updateDoc, doc } from 'firebase/firestore'
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore'
+
 import { toast } from 'react-toastify'
+
+import ListingItem from '../components/ListingObject'
+
 import arrowRight from '../assets/svg/kbArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
   const auth = getAuth()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
   // ! For edit/change details
   const [changeDetails, setChangeDetails] = useState(false)
   // ! check if user is login
@@ -20,6 +36,35 @@ function Profile() {
   const { name, email } = formData
   // ! import navigate
   const navigate = useNavigate()
+  // ! Function to get Data from collection DB using async
+  useEffect(() => {
+    const fetchUserListing = async () => {
+      const listingsRef = collection(db, 'listings')
+      // ! Store reference in a variable
+      const qry = query(
+        listingsRef,
+        // ! Check if same with userID, display timestamp
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      )
+      // ! Pass query to a variable after initialize
+      const queryDoc = await getDocs(qry)
+      // ! Initialize with array
+      let listings = []
+
+      queryDoc.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+    fetchUserListing()
+  }, [auth.currentUser.uid])
+
   // ! create button for signing out
   const onLogout = () => {
     auth.signOut()
@@ -54,6 +99,20 @@ function Profile() {
       [e.target.id]: e.target.value,
     }))
   }
+  // ! Function for Delete
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure to remove the item?')) {
+      // ! access listings from DB - firebase using ID, process await
+      await deleteDoc(doc(db, 'listings', listingId))
+      const updatedListings = listings.filter(
+        (listing) => listing !== listingId
+      )
+      setListings(updatedListings)
+      toast.success('You have remove your listing')
+    }
+  }
+  // ! Function for Edit
+  const onEdit = () => {}
 
   // ! display user name if login
   return (
@@ -80,7 +139,6 @@ function Profile() {
             {changeDetails ? 'done' : 'change'}
           </p>
         </div>
-
         <div className='profileCard'>
           <form>
             <input
@@ -101,12 +159,28 @@ function Profile() {
             />
           </form>
         </div>
-
         <Link to='/create-listing' className='createListing'>
           <img src={homeIcon} alt='home' />
           <p>List your property</p>
           <img src={arrowRight} alt='arrow right' />
         </Link>
+        {/* // ! Display data information - w - Delete / Edit */}
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your Listings</p>
+            <ul className='listingsList'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
