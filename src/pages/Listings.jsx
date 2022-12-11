06797e73af4c80ce44a0e62f672a52a199f54950
ggcldev <ggcldev@gmail.com>
2022-12-12 01:43:14 +0800

    Imported CSS styles for swiper

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -6,9 +6,14 @@ import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-import 'swiper/css'
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listings() {
