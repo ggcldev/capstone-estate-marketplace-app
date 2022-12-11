9ca8d78fbd0e2c6d6f32836e1612bd43df9aea00
ggcldev <ggcldev@gmail.com>
2022-12-11 18:42:30 +0800

    Store and display listings

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -29,7 +29,83 @@ function Listings() {
     fetchListing()
   }, [navigate, params.listingId])
 
-  return <div>Listings</div>
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    <main>
+      {/* SLIDER */}
+      {/* Option to share the listing */}
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt='Icon share' />
+      </div>
+      {/* If shared link copy is true then share the copied data from DB */}
+      {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+      {/* Display listing details */}
+      <div className='listingDetails'>
+        {/* Display listing with condition : check if there's discounted price */}
+        <p className='listingName'>
+          {listing.name} - PHP
+          {listing.offer
+            ? //  ! Added Regex for format
+              listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          {/* Display type Sale ? Rent */}
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {/* Check if it's a regular ? discounted price and will display the price */}
+        {listing.offer && (
+          <p className='discountPrice'>
+            PHP{listing.regularPrice - listing.discountedPrice}Discount
+          </p>
+        )}
+        {/* Display available bedrooms, bathrooms, etc. */}
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : '1 Bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathroom`
+              : '1 Bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <div className='listingLocationTitle'>Location</div>
+        {/* MAP */}
+        {/* Check if user is not signed in and nor it's listing, able to send a contact form */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className='primaryButton'
+          >
+            Contact Property Owner / Agent
+          </Link>
+        )}
+      </div>
+    </main>
+  )
 }
 
 export default Listings
