193711a2f8af4d2353ab97e5671bb257829f4bc8
ggcldev <ggcldev@gmail.com>
2022-12-11 16:59:25 +0800

    Fixed some errors on the console; Adding eslint comment, and switch default; Initiate Fetch single listing

--- src/pages/CreateNewListing.jsx
+++ src/pages/CreateNewListing.jsx
@@ -14,6 +14,7 @@ import { toast } from 'react-toastify'
 import { v4 as uuidv4 } from 'uuid'
 
 function CreateNewListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
   const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
@@ -143,6 +144,7 @@ function CreateNewListing() {
               case 'running':
                 console.log('Upload is running')
                 break
+              default: // do nothing
             }
           },
           (error) => {
