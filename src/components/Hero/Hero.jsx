fa1622e881c26a619e565258e17317384789542f
ggcldev <ggcldev@gmail.com>
2022-12-12 21:01:06 +0800

    switched to flexbox

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,10 @@ function Hero() {
   return (
     <div className='hero'>
       <div className='hero-container'>
-        <h1 className='h1-hero'>Real Estate Listing Co.</h1>
-        <h2 className='h2-hero'>Your only marketplace search for home</h2>
+        <div className='hero-heading'>
+          <h1 className='h1-hero'>Real Estate Listing Co.</h1>
+          <h2 className='h2-hero'>Your only marketplace search for home</h2>
+        </div>
       </div>
     </div>
   )
